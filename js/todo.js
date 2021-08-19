bbb44c5e1bc8809b08d6e0d32f3dab089c475803
seonzone <tjdwhd2247@gmail.com>
2021-08-20 00:29:42 +0900

    각 Item에 ID값을 부여

--- js/todo.js
+++ js/todo.js
@@ -11,13 +11,15 @@ function saveToDos() {
 
 function deleteToDo(event) { 
     const li = event.target.parentElement;
+    
     li.remove();
 }
 
 function paintToDo(newTodo) { 
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click",deleteToDo);
@@ -30,8 +32,12 @@ function handleToDoSubmit(event) {
      event.preventDefault();
      const newTodo = toDoInput.value;
      toDoInput.value="";
-     toDos.push(newTodo);
-     paintToDo(newTodo);
+     const newToDoObj = { 
+         text: newTodo,
+         id: Date.now(),
+     }
+     toDos.push(newToDoObj);
+     paintToDo(newToDoObj);
      saveToDos();
      
 
